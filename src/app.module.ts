ed1e334c2fdb534576015738b979910599e72dda
wuzhengquan <wuzhengquan@zhixueyun.com>
2022-02-15 17:26:00 +0800

    db update

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,11 @@ import envConfig from '../config/env';
       useFactory: async (configService: ConfigService) => ({
         type: 'mysql', // 数据库类型
         entities: [PostsEntity], // 数据表实体
-        host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
-        port: configService.get<number>('DB_PORT', 3306), // 端口号
-        username: configService.get('DB_USER', 'root'), // 用户名
-        password: configService.get('DB_PASSWORD', 'root'), // 密码
-        database: configService.get('DB_DATABASE', 'blog'), //数据库名
+        host: configService.get('DB_HOST'), // 主机，默认为localhost
+        port: configService.get<number>('DB_PORT'), // 端口号
+        username: configService.get('DB_USER'), // 用户名
+        password: configService.get('DB_PASSWORD'), // 密码
+        database: configService.get('DB_DATABASE'), //数据库名
         timezone: '+08:00', //服务器上配置的时区
         synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
       }),
