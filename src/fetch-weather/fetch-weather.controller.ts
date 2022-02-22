b074b0aaebb4294f733141a82ea1a3ed79e6f069
wuzhengquan <wuzhengquan@zhixueyun.com>
2022-02-22 19:09:36 +0800

    db update

--- src/fetch-weather/fetch-weather.controller.ts
+++ src/fetch-weather/fetch-weather.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { FetchWeatherService } from './fetch-weather.service';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 
-@Controller('fetch-weather')
-export class FetchWeatherController {}
+@Controller('weather')
+export class FetchWeatherController {
+  constructor(private readonly fetchWeatherService: FetchWeatherService) {}
+
+  @Post()
+  async create(@Body() post) {
+    return await this.fetchWeatherService.create(post);
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id) {
+    return await this.fetchWeatherService.findById(id);
+  }
+}
--- src/fetch-weather/fetch-weather.entity.ts
+++ src/fetch-weather/fetch-weather.entity.ts
@@ -1,10 +1,13 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('fWeather')
+@Entity('weather')
 export class FetchWeatherEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 50 })
+  @Column({ length: 100 })
   title: string;
+
+  @Column('json')
+  forecast: string;
 }
